971caf2f34f13a259d3a45601e71dbc7514b0d94
marryedoceria@gmail.com <alexxxfmesquita21@gmail.com>
2024-05-30 10:11:29 -0300

    Operadores Relacionais

--- tipos-variáveis/src/Operadores.java
+++ tipos-variáveis/src/Operadores.java
@@ -1,16 +1,37 @@
 public class Operadores {
 
    public static void main(String[] args) {
-   int a, b;  
-   a = 5;
-   b = 6;
+   String nomeUm = "Alexandre";
+   String nomeDois = new String("Alexandre");
 
-   int resultado = a==b ? 1 : 0;
-   
+   System.out.println(nomeUm .equals(nomeDois));
 
-       System.out.println(resultado);
+
+
+
+
+   int numero1 = 1;
+   int numero2 = 2;
+
+   boolean simNao = numero1 == numero2; 
+
+   if (numero1 < numero2) {
+
+      System.out.println("A nossa condição é verdadeira");
 
       
+   }
+   
+
+       System.out.println("numeroUm é igual ao numeroDois?" + simNao);
+
+       simNao = numero1 != numero2;
+
+       System.out.println("numeroUm é diferente do numeroDois?" + simNao);
+
+       simNao = numero1 > numero2;
+
+       System.out.println("numeroUm é maior do que o numeroDois?" + simNao);
 
     
    }
